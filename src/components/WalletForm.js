718e9a012defdbc134fdb6988751568b0893a62e
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-02-02 21:10:54 -0300

    Requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,11 +1,116 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { apiCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
+  state = {
+    value: '',
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+  };
+
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    await dispatch(apiCurrencies());
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { value, description, currency, method, tag } = this.state;
+    const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="value">
+          Valor:
+          <input
+            type="text"
+            id="value"
+            name="value"
+            data-testid="value-input"
+            value={ value }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição:
+          <input
+            type="text"
+            id="description"
+            name="description"
+            data-testid="description-input"
+            value={ description }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <select
+          name="currency"
+          id="currency"
+          data-testid="currency-input"
+          value={ currency }
+          onChange={ this.handleChange }
+        >
+          {currencies
+            .map((curr) => <option key={ curr } value={ curr }>{ curr }</option>)}
+        </select>
+
+        <label htmlFor="method">
+          Método de pagamento:
+          <select
+            name="method"
+            id="method"
+            data-testid="method-input"
+            value={ method }
+            onChange={ this.handleChange }
+          >
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          <select
+            name="tag"
+            id="tag"
+            data-testid="tag-input"
+            value={ tag }
+            onChange={ this.handleChange }
+          >
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <button
+          type="button"
+          // disabled={ !(isValideEmail === true && isValidePassword === true) }
+          // onClick={ () => this.saveEmail(email) }
+        >
+          Adicionar despesa
+        </button>
+      </form>
     );
   }
 }
 
-export default WalletForm;
+WalletForm.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+export default connect(mapStateToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,25 @@
 // Coloque aqui suas actions
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const addEmail = (payload) => ({
   type: ADD_EMAIL,
   payload,
 });
+
+export const getCurrencies = (payload) => ({
+  type: GET_CURRENCIES,
+  payload,
+});
+// Atualização do estado local na WalletForm está intermitente, mesmo que o estado global está sempre atualizado.
+export function apiCurrencies() {
+  return (dispatch) => {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((data) => {
+        const allKeys = Object.keys(data);
+        dispatch(getCurrencies(allKeys.filter((key) => key !== 'USDT')));
+      });
+    // .catch((error) => dispatch(requestFailed(error)));
+  };
+}
