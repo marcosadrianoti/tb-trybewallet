fbd21723071303ec2fab3ac1abd1d1f0424a0a5a
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-02-04 12:40:00 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import '../styles/table.css';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <div>Table</div>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          <tr>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+            <td>.</td>
+          </tr>
+        </table>
+      </>
     );
   }
 }
